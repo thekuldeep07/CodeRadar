f53a77fcec4a644de323b21722982159fff7b7a8
thekuldeep07 <thekuldeep07@gmail.com>
2021-11-23 10:21:46 +0530

    end Date2

--- app/src/main/java/com/example/coderadar/presentation/adapter/ContestAdapter.kt
+++ app/src/main/java/com/example/coderadar/presentation/adapter/ContestAdapter.kt
@@ -7,9 +7,11 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.AsyncListDiffer
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.coderadar.data.model.Contest
 import com.example.CodeRadar.databinding.ContestListItemBinding
+import com.example.coderadar.data.model.Contest
+import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 
 class ContestAdapter: RecyclerView.Adapter<ContestAdapter.contestViewHolder>() {
 
@@ -26,19 +28,20 @@ class ContestAdapter: RecyclerView.Adapter<ContestAdapter.contestViewHolder>() {
     }
 
 
-    val  differ = AsyncListDiffer(this,callback)
-
-
-
+    val  differ = AsyncListDiffer(this, callback)
+    
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): contestViewHolder {
-        val  binding = ContestListItemBinding.inflate(LayoutInflater.from(
-                parent.context),parent,false)
+        val  binding = ContestListItemBinding.inflate(
+            LayoutInflater.from(
+                parent.context
+            ), parent, false
+        )
         return  contestViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: contestViewHolder, position: Int) {
         val contest=differ.currentList[position]
-        Log.d("kul",""+contest.href)
+        Log.d("kul", "" + contest.href)
         holder.bind(contest)
     }
 
@@ -46,39 +49,47 @@ class ContestAdapter: RecyclerView.Adapter<ContestAdapter.contestViewHolder>() {
         return differ.currentList.size
     }
 
-    inner class  contestViewHolder(val binding : ContestListItemBinding)
+    inner class  contestViewHolder(val binding: ContestListItemBinding)
         :RecyclerView.ViewHolder(binding.root){
-        fun bind(contest: Contest){
-            binding.eventTitle.text=contest.event
+            fun bind(contest: Contest){
+                binding.eventTitle.text=contest.event
 
 
-            val startDate = LocalDateTime.parse(contest.start)
-            val endDate = LocalDateTime.parse(contest.end)
-            val startTime = (startDate.hour.toString() + ":" + startDate.minute.toString())
-            val endTime = (endDate.hour.toString() + ":" + endDate.minute.toString())
-            binding.startDate.text=(startDate.dayOfMonth.toString() +
-                    "-"+startDate.month.toString()+"-"+startDate.year.toString())
-            binding.endDate.text=(endDate.dayOfMonth.toString() +
-                    "-"+endDate.month.toString()+"-"+endDate.year.toString())
+                val startDate = LocalDateTime.parse(contest.start)
+                val endDate = LocalDateTime.parse(contest.end)
+                val startTime = (startDate.hour.toString() + ":" + startDate.minute.toString())
+                val endTime = (endDate.hour.toString() + ":" + endDate.minute.toString())
+                binding.startDate.text=(startDate.dayOfMonth.toString() +
+                        "-"+startDate.month.toString()+"-"+startDate.year.toString())
+                binding.endDate.text=(endDate.dayOfMonth.toString() +
+                        "-"+endDate.month.toString()+"-"+endDate.year.toString())
 
-            binding.startTime.text = startTime.toString()
-            binding.endTime.text = endTime.toString()
+                binding.startTime.text = startTime.toString()
+                binding.endTime.text = endTime.toString()
 
-            val currentDate = LocalDateTime.now()
-            if (startDate.isAfter(currentDate)){
-                binding.statusTv.text = "Upcoming"
-                binding.statusTv.setTextColor(Color.RED)
-            }
-            else{
-                if (currentDate.isBefore(endDate)){
-                    binding.statusTv.text = "Live"
-                    binding.statusTv.setTextColor(Color.GREEN)
+                val currentDate = LocalDateTime.now()
+//                if (( currentDate.compareTo(startDate) > 0 ) &&(currentDate.compareTo(endDate)<0)){
+//                    binding.statusTv.text = "Live"
+//                    binding.statusTv.setTextColor(Color.GREEN)
+//
+//                    //  0 comes when two date are same,
+//                    //  1 comes when date1 is higher then date2
+//                    // -1 comes when date1 is lower then date2
+//
+//                }
+                if ((currentDate.isBefore(startDate))){
+                    binding.statusTv.text = "upcoming"
+                    binding.statusTv.setTextColor(Color.RED)
                 }
+                else{
+                    if(endDate.isAfter(currentDate)){
+                        binding.statusTv.text = "live"
+                        binding.statusTv.setTextColor(Color.GREEN)
+                    }
 
 
+                }
             }
-        }
-
     }
 
 }
